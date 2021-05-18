2c07c74579ac7c941bb47d2e628415060153b666
Софья Качулина <sof.gilyazeva@yandex.ru>
2021-05-18 13:16:30 +0500

    Added ViewModels for Category and Product

---
+++ ViewModel/CategoryViewModel.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Windows.Data;
+
+namespace TreeViewSample
+{
+    public class CategoryViewModel
+    {
+        private readonly Category Category;
+
+        public string Name => Category.Name;
+
+        List<CategoryViewModel> Subcategories => Category.Subcategories?.Select(x => new CategoryViewModel(x)).ToList();
+
+        List<ProductViewModel> Products => Category.Products?.Select(x => new ProductViewModel(x)).ToList();
+
+        /// <summary>
+        /// Union of different classes colections
+        /// </summary>
+        public IList Children => new CompositeCollection
+        {
+            new CollectionContainer { Collection = Products },
+            new CollectionContainer { Collection = Subcategories },
+        };
+
+        public CategoryViewModel(Category category)
+        {
+            Category = category;
+        }
+    }
+}
---
+++ ViewModel/ProductViewModel.cs
@@ -0,0 +1,13 @@
+﻿namespace TreeViewSample
+{
+    public class ProductViewModel
+    {
+        public readonly Product Product;
+        public string Name => Product.Name;
+
+        public ProductViewModel(Product product)
+        {
+            Product = product;
+        }
+    }
+}
